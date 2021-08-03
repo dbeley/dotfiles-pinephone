25ca989a747f1e77ec79ccbbf6d38d97c85e72fb
dbeley <6568955+dbeley@users.noreply.github.com>
2021-08-03 11:25:20 +0200

    Update firefox, fish, screenshot2app.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,6 +20,7 @@ alias cpr "rsync -azvhP --stats --inplace --zc=zstd --zl=3"
 alias expacsize "expac -H M '%m\t%n' | sort -h"
 alias expaclist "expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort -h"
 
+alias mpv360 "mpv --ytdl-format=\"(bestvideo[height<=360]+bestaudio)[ext=webm]/bestvideo[height<=360]+bestaudio/best[height<=360]/bestvideo+bestaudio/best\" "
 alias mpv720 "mpv --ytdl-format=\"(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best\" "
 alias mpv1080 "mpv --ytdl-format=\"(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best\" "
 
